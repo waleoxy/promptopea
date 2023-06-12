455c60bbd9eb6a28ca599fe9d131b492fd05b1ec
Olawale Otubu <waleoxy@gmail.com>
2023-06-13 00:54:33 +0100

    fix:revert

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -24,7 +24,7 @@ const Feed = () => {
   const [posts, setPosts] = useState([]);
 
   const handleSearchChange = (e) => {
-
+    clearTimeout(searchTimeout);
     setSearchText(e.target.value);
 
     setSearchTimeout(
@@ -33,8 +33,6 @@ const Feed = () => {
         setSearchedResult(searchResult);
       }, 500)
     );
-
-    clearTimeout(searchTimeout);
   };
 
   const handleTagClick = (tagName) => {
